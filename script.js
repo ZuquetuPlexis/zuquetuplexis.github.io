2624e6ac9fb3ad60c1ff12c538f66279d86a43d8
Tom Jannek <tom.jannek@hacrafu.de>
2023-12-28 16:53:05 +0100

    chore: Add leaflet sidebar

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var map = L.map('map').setView([52.53028, 13.79417], 13);
 
+var controlSidebar = L.control.sidebar('side-bar').addTo(map);
+controlSidebar.show();
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
