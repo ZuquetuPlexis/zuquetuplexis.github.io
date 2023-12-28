7d1c241dc09c6c3f45079e2c6d46d1250ead69b7
Tom Jannek <tom.jannek@hacrafu.de>
2023-12-28 16:58:29 +0100

    chore: Update leaflet sidebar

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var map = L.map('map').setView([52.53028, 13.79417], 13);
 
-var controlSidebar = L.control.sidebar('side-bar').addTo(map);
-controlSidebar.show();
+var sidebar = L.control.sidebar({
+    autopan: false,       // whether to maintain the centered map point when opening the sidebar
+    closeButton: true,    // whether t add a close button to the panes
+    container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+    position: 'left',     // left or right
+}).addTo(map);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
