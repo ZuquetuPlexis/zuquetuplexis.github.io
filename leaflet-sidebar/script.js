7733169029291b7b4d440d739752dce58d923a66
Tom Jannek <tom.jannek@hacrafu.de>
2023-12-30 16:39:45 +0100

    Update script.js

--- leaflet-sidebar/script.js
+++ leaflet-sidebar/script.js
@@ -5,39 +5,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var sidebar = L.control.sidebar({
-		autopan: false,
-		//closeButton: true,
-		container: 'sidebar',
-		position: 'left'
-}).addTo(map);
-
-var panelBus = {
-	id: 'bus_stop_quality',
-	tab: 'LF',
-	pane: 'lf-sb',
-	title: 'Übersicht Qualität',
-	position: 'top'
-};
-
-var panelSetting = {
-	id: 'setting',
-	tab: 'stt',
-	pane: 'sttg',
-	title: 'setting stuff',
-	position: 'top'
-};
-
-var panelGit = {
-	id: 'git',
-	tab: 'git',
-	pane: 'git-hub',
-	title: 'git bits',
-	position: 'top'
-};
-
-sidebar.addPanel(panelBus);
-sidebar.addPanel(panelSetting);
-sidebar.addPanel(panelGit);
-
-sidebar.open('bus_stop_quality');
+var sidebar = L.control.sidebar('sidebar').addTo(map);
