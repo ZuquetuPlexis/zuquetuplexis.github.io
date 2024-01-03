1f85ce54ffa2d48e1ae7b26f3c1da612f9564602
Tom Jannek <tom.jannek@hacrafu.de>
2024-01-03 21:12:30 +0100

    Update script.js

--- dodo/script.js
+++ dodo/script.js
@@ -73,7 +73,7 @@ const busQualChart = new Chart('bus_stop_qual', {
 const overpassURL = "https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%28area%5B%22de%3Aamtlicher_gemeindeschluessel%22%3D%2212064380%22%5D-%3E.dodo%3Bnode%5Bhighway%3Dbus_stop%5D%28area.dodo%29%3B%29%3Bout%20geom%3B";
 
 function updateBusChart(nodeTags) {
-
+	console.log(chartData.datasets[0].data);
 }
 
 function nodePopupText(nodeTags) {
@@ -99,6 +99,11 @@ async function getBusStops() {
 		if (node.type == "node") {
 			L.marker([node.lat, node.lon], {icon: busIcon}).addTo(map).bindPopup(nodePopupText(node.tags));
 		}
+
+		//updateBusChart(node.tags);
+		setTimeout(() => {
+			updateBusChart(node.tags);
+		}, 250);
 	}
 }
 
