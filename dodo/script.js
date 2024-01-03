4e12e2c12b8bf2311092d453bf4088bd77cd4f4c
Tom Jannek <tom.jannek@hacrafu.de>
2024-01-03 18:35:09 +0100

    Update script.js

--- dodo/script.js
+++ dodo/script.js
@@ -1,6 +1,18 @@
+// Leaflet
+
 var map = L.map('map').setView([52.53028, 13.79417], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		maxZoom: 19,
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// overpass api
+
+const overpassURL = "https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%28area%5B%22de%3Aamtlicher_gemeindeschluessel%22%3D%2212064380%22%5D-%3E.dodo%3Bnode%5Bhighway%3Dbus_stop%5D%28area.dodo%29%3B%29%3Bout%20geom%3B";
+
+async function getBusStops() {
+	const response = await fetch(overpassURL);
+	const busStops = await response.json();
+	console.log(busStops);
+}
