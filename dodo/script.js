cfabf885a581bca7f68f0c5d7a35b1b683a2c522
Tom Jannek <tom.jannek@hacrafu.de>
2024-01-03 20:57:31 +0100

    Update script.js

--- dodo/script.js
+++ dodo/script.js
@@ -14,37 +14,6 @@ var busIcon = L.divIcon({
 	className: 'busIcon'
 });
 
-
-// ----- ----- -----
-// overpass api
-// ----- ----- -----
-
-const overpassURL = "https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%28area%5B%22de%3Aamtlicher_gemeindeschluessel%22%3D%2212064380%22%5D-%3E.dodo%3Bnode%5Bhighway%3Dbus_stop%5D%28area.dodo%29%3B%29%3Bout%20geom%3B";
-
-async function getBusStops() {
-	const response = await fetch(overpassURL);
-	const busStops = await response.json();
-
-	for (let i in busStops.elements) {
-		let node = busStops.elements[i];
-
-		if (node.type == "node") {
-			let popupText = '';
-
-			popupText += `name: ${node.tags.name ? node.tags.name : ''}<br>`;
-			popupText += `sitzbank: ${node.tags.bench ? 'ja' : 'nein'}<br>`;
-			popupText += `mülleimer: ${node.tags.bin ? 'ja' : 'nein'}<br>`;
-			popupText += `beleuchtet: ${node.tags.lit ? 'ja' : 'nein'}<br>`;
-			popupText += `überdacht: ${node.tags.shelter ? 'ja' : 'nein'}<br>`;
-			popupText += `taktile oberfläche: ${node.tags.tactile_paving ? 'ja' : 'nein'}<br>`;
-			
-			L.marker([node.lat, node.lon], {icon: busIcon}).addTo(map).bindPopup(popupText);
-		}
-	}
-}
-
-getBusStops();
-
 // ----- ----- -----
 // chart js
 // ----- ----- -----
@@ -80,3 +49,41 @@ const busQualChart = new Chart('bus_stop_qual', {
 		}
 	}
 });
+
+// ----- ----- -----
+// overpass api
+// ----- ----- -----
+
+const overpassURL = "https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%28area%5B%22de%3Aamtlicher_gemeindeschluessel%22%3D%2212064380%22%5D-%3E.dodo%3Bnode%5Bhighway%3Dbus_stop%5D%28area.dodo%29%3B%29%3Bout%20geom%3B";
+
+function updateBusChart(nodeTags) {
+
+}
+
+function nodePopupText(nodeTags) {
+	let popupText = '';
+
+	popupText += `name: ${nodeTags.name ? nodeTags.name : ''}<br>`;
+	popupText += `sitzbank: ${nodeTags.bench ? 'ja' : 'nein'}<br>`;
+	popupText += `mülleimer: ${nodeTags.bin ? 'ja' : 'nein'}<br>`;
+	popupText += `beleuchtet: ${nodeTags.lit ? 'ja' : 'nein'}<br>`;
+	popupText += `überdacht: ${nodeTags.shelter ? 'ja' : 'nein'}<br>`;
+	popupText += `taktile oberfläche: ${nodeTags.tactile_paving ? 'ja' : 'nein'}`;
+
+	return popupText;
+}
+
+async function getBusStops() {
+	const response = await fetch(overpassURL);
+	const busStops = await response.json();
+
+	for (let i in busStops.elements) {
+		let node = busStops.elements[i];
+
+		if (node.type == "node") {
+			L.marker([node.lat, node.lon], {icon: busIcon}).addTo(map).bindPopup(nodePopupText(node.tags));
+		}
+	}
+}
+
+getBusStops();
